df0a174b28e50463d32926b8bcd410eb62a95a75
dev mohamed gouda <dev.mohamedgouda@gmail.com>
2019-11-21 13:24:06 +0200

    task 1 for mostafa and youssef

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.main')
+
+@section('content')
+
+<!-- This Page for Mostafa Samy -->
+<!-- write your Code Here -->
+
+
+
+@endsection
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,26 @@
+<!--
+
+        This Page For Youseef
+        Write Your Code Here
+
+-->
+
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="{{asset('css/app.css')}}">
+
+        <title>IEEE-SHA</title>
+        
+       
+    </head>
+    <body>
+     
+        @yield('content')
+
+        <script src="{{asset('js/app.js')}}"></script>
+
+    </body>
+</html>
