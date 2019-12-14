f10c2bcca8517623611ea66303e7f717297b9f6a
dev mohamed gouda <dev.mohamedgouda@gmail.com>
2019-12-14 14:15:33 +0200

    csrf

--- resources/views/home/regstration.blade.php
+++ resources/views/home/regstration.blade.php
@@ -4,8 +4,15 @@
 
 <!-- This Page for Mona -->
 <!-- To open this page just write serve link with regstrtion : 127.0.0.1:8000/regstrtion -->
+
+<form>
+    @csrf
 <!-- write your Code Here -->
 
 
+
+</form>
+
+{{-- mona --}}
 
 @endsection
