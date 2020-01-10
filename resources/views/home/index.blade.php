287d9ba282170aff1efccc7257b89ca4c070727f
YousefM0060 <myousef049@gmail.com>
2020-01-10 21:31:54 +0200

    update front

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -6,6 +6,98 @@
 <!-- To open this page just write serve link without anything -->
 <!-- write your Code Here -->
 
+<section>
+            
+        <div class="container">
+<!--frist row-->
+            <div class="row e">
+<!--frist column (logo)-->
+                <div class="col-md-3 col-sm-12">logo</div>
+<!--end of frist column-->
+<!--second column-->
+                <div class="col-md-9 col-sm-12">
+                <div class="row a"><h3 class="b">General Information About IEEE:</h3> </div>
+                <div class="row c">
+                    <p>
+                    -IEEE, An association dedicated to advancing innovation and 
+                    technological excellence for the benefit of humanity, 
+                    is the world’s largest technical professional society.
+                    It is designed to serve professionals involved in all aspects 
+                    of the electrical, electronic, and computing fields and related 
+                    areas of science and technology that underlie modern civilization.
+                    </p>
+                    </div>
+                </div>
+<!--end of second column-->
+            </div>
+<!--end of frist row-->
+            
+<!--second row-->
+                <div style="background-color: #f1f5fe" class="row e">
+<!--frist column-->
+                    <div class="col-md-4 col-sm-12 d"><i style="color: #FEC459" class="fas fa-lightbulb f"></i></div>
+<!--end of frist column-->
+<!--second column-->
+                    <div class="col-md-4 col-sm-12 d"><i style="color: #0170A8" class="fas fa-user-graduate f"></i></div>
+<!--end of second column-->
+<!--third column-->
+                    <div class="col-md-4 col-sm-12 d"><i style="color: #FDC39B" class="fas fa-hands f"></i></div>
+<!--end of third column-->
+                </div>
+<!--end of second row-->
+            
+<!--third row-->
+            <div class="row e">
+<!--frist column (logo)-->
+                <div class="col-md-3 col-sm-12">logo</div>
+<!--end of frist column-->
+<!--second column-->
+                <div class="col-md-9 col-sm-12">
+                <div class="row a"><h3 class="b">About IEEE Egypt Section:</h3> </div>
+                <div class="row c">
+                    <p>
+                    -The IEEE is divided into different categories according to
+                    geographical regions, technical aspects, and expertise.
+                    Geographically, The IEEE divides the world into 10 regions.
+                    IEEE Egypt Section lies in Region 8, which includes Europe sections, Africa,
+                    and Middle East Sections.
+                    </p>
+                    </div>
+                </div>
+<!--end of second column-->
+            </div>
+<!--end of third row-->
+            
+<!--fourth row-->
+            <div style="background-color: #f1f5fe" class="row e">
+<!--frist column (logo)-->
+                <div class="col-md-3 col-sm-12">logo</div>
+<!--end of frist column-->
+<!--second column-->
+                <div class="col-md-9 col-sm-12">
+                    
+                    <div class="row c">
+                    <p>
+                   -To lead in getting the electrical power and energy engineering
+                    students ready to work professionally in the industry..  
+                    </p>
+                    </div>
+                    
+                <div class="row c">
+                    <p>
+                   -To keep adding value and providing students
+                    with the technicalities and scientific information
+                    needed to get into the electrical power and energy industry ready.  
+                    </p>
+                    </div>
+                    
+                </div>
+<!--end of second column-->
+            </div>
+<!--end of fourth row-->
+            </div>
+        </section>
+
 index page
 
 @endsection
